fc47ea9a11680221827846162a9a5ebd980531dd
Khasan Rashidov <khasanrashidov2002@gmail.com>
2023-07-22 19:55:55 +0500

    linq: first, last, single...

--- 009. Lambda, LINQ/073. LINQ Task (CSV parsing) Chess Players/Program.cs
+++ 009. Lambda, LINQ/073. LINQ Task (CSV parsing) Chess Players/Program.cs
@@ -25,6 +25,52 @@ namespace LINQ
             Console.WriteLine($"The highest rating in TOP 10 is {playersList.Max(player => player.Rating)}");
             Console.WriteLine($"The average rating in TOP 10 is {playersList.Average(player => player.Rating):F0}");
             Console.WriteLine($"The sum of ratings in TOP 10 is {playersList.Sum(player => player.Rating)}");
+
+            // First() and Last() throw exception if there is no such element
+            // FirstOrDefault() and LastOrDefault() return default value if there is no such element
+            // Single() and SingleOrDefault() throw exception if there is more than one element
+            // ElementAt() returns element at specified position
+            // ElementAtOrDefault() returns default value if there is no such element
+            // Count() returns number of elements
+            // Any() returns true if there is at least one element
+            // All() returns true if all elements satisfy the condition
+            // Contains() returns true if there is an element that equals to the specified value
+            // Distinct() returns distinct elements
+            
+            // First() and Last()
+            Console.WriteLine($"USA players count is {playersList.Count(player => player.Country == "USA")}");
+            Console.WriteLine($"First USA player is {playersList.First(player => player.Country == "USA")}");
+            Console.WriteLine($"Last USA player is {playersList.Last(player => player.Country == "USA")}");
+
+            // FirstOrDefault()
+            Console.WriteLine($"First Russian player is {playersList.FirstOrDefault(player => player.Country == "RUS")}");
+            Console.WriteLine($"Last Russian player is {playersList.LastOrDefault(player => player.Country == "RUS")}");
+
+            // Just calling First() and Last() without any parameters returns first and last elements
+            Console.WriteLine($"First player is {playersList.First()}");
+            Console.WriteLine($"Last player is {playersList.Last()}");
+
+            // Just calling FirstOrDefault() and LastOrDefault() without any parameters returns first and last elements or null
+            Console.WriteLine($"First player is {playersList.FirstOrDefault()}");
+            Console.WriteLine($"Last player is {playersList.LastOrDefault()}");
+
+            ChessPlayer firstPlayer = playersList.First();
+            Console.WriteLine($"First player is {firstPlayer.FirstName} {firstPlayer.LastName}");
+
+            // Single() and SingleOrDefault()
+            Console.WriteLine($"The only player from Norway is {playersList.Single(player => player.Country == "NOR")}");
+            Console.WriteLine($"The only player from Norway is {playersList.SingleOrDefault(player => player.Country == "NOR")}");
+
+            // As there are two players from USA, both Single() and SingleOrDefault() throw exception
+            //Console.WriteLine($"The only player from USA is {playersList.SingleOrDefault(player => player.Country == "USA")}");
+
+            // ElementAt() and ElementAtOrDefault()
+            Console.WriteLine($"The 3rd player is {playersList.ElementAt(2)}");
+            Console.WriteLine($"The 3rd player is {playersList.ElementAtOrDefault(2)}");
+
+            // Any() and All() return bool
+            Console.WriteLine($"Are there any players from China? {playersList.Any(player => player.Country == "CHN")}");
+            Console.WriteLine($"Are all players from Russia? {playersList.All(player => player.Country == "RUS")}");
         }
     }
 }
